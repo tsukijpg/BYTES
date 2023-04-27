7b2dc56c37825a73a4081c37492e5dc3e81a8e76
tsukijpg <129511544+tsukijpg@users.noreply.github.com>
2023-04-27 14:59:30 +0100

    Update MatrizQuadradaDiagonalSecundaria.js

--- MatrizQuadradaDiagonalSecundaria.js
+++ MatrizQuadradaDiagonalSecundaria.js
@@ -1,11 +1,11 @@
 function somaDiagonalSecundaria(matrizquadrada) {
     let soma = 0;
     for (let i = 0; i < matrizquadrada.length; i++) {
-        soma += matrizquadrada[i][matrizquadrada.length - 1 - i] //quero adicionar á soma a posiçao de i + o fim do array menos 1 para achar a ultima posição
+        soma += matrizquadrada[i][matrizquadrada.length - 1 - 1] //quero adicionar á soma a posiçao de i + o fim do array menos 1 para achar a ultima posição
     }
 
     return soma
-    // matrizquadrada[0][3 - 0    3] // começa na posiçao 0..... encontra o 3 que é o fim.....
+    // matrizquadrada[0][3 - 0    3] // começa na posiçao 0..... encontra o 3 que é o fim..... tira um que está a mais no comprimento.... tiro o valor de i 
     // matrizquadrada[1][3 - 1    2]
     // matrizquadrada[2][3 - 2    1]
     // matrizquadrada[3][3 - 3    0]
