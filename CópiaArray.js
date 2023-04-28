0988b96ab9c7bd337275eaaa38fcbdb1208d2d3d
tsukijpg <129511544+tsukijpg@users.noreply.github.com>
2023-04-28 09:45:55 +0100

    Create CópiaArray.js

---
+++ CópiaArray.js
@@ -0,0 +1,8 @@
+function primeiraArray(array)
+let novaArray = array.slice().sort((a,b) => a-b);
+return novaArray
+
+
+para criar uma nova array apartir de outra, primeiro tenho q usar o slice para fazer uma cópia.
+só depois posso usar o sort para ordenar.
+o a e o b não percebi.
